ac7d7691b9e886bda124429234437a5ea92c69bb
RickFerry <rick_ferry1@hotmail.com>
2023-05-04 19:35:39 -0300

    Cors

--- src/main/java/br/com/ferry/financeapi/config/security/WebSecurityConfig.java
+++ src/main/java/br/com/ferry/financeapi/config/security/WebSecurityConfig.java
@@ -12,11 +12,23 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import br.com.ferry.financeapi.config.security.service.SecurityFilter;
 
 @Configuration
 public class WebSecurityConfig{
+
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("/**");
+            }
+        };
+    }
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http, @Autowired SecurityFilter securityFilter) throws Exception {
